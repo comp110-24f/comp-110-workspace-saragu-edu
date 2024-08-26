8e27810f1ce796f59b2df8e7a1d9b514b1ab283c
Sara <saragu@unc.edu>
2024-08-26 12:13:12 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730758554"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
